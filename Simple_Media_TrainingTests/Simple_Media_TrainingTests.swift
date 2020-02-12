7239cd5f2dac0ab9154a7cd35e85ff8e74a05d07
Akshay Shinde <ednihs.yahska@gmail.com>
2020-02-11 20:02:50 -0800

    Initial Commit

---
+++ Simple_Media_TrainingTests/Simple_Media_TrainingTests.swift
@@ -0,0 +1,34 @@
+//
+//  Simple_Media_TrainingTests.swift
+//  Simple_Media_TrainingTests
+//
+//  Created by Akshay Shinde on 2/11/20.
+//  Copyright © 2020 Akshay_Shinde. All rights reserved.
+//
+
+import XCTest
+@testable import Simple_Media_Training
+
+class Simple_Media_TrainingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
